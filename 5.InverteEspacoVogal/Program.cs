ea1fc23f7ea3b6f9c403fcbb8420d268d44c2f61
luisr <luisr.silva@outlook.com>
2023-01-05 19:43:48 -0300

    Adicionando nova solução para método que manipula inverte string e dá espaço em vogal

---
+++ 5.InverteEspacoVogal/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma palavra");
+        string? word = Console.ReadLine();
+        if (word != null) Console.WriteLine(ReverseString(word));
+    }
+
+    static string ReverseString(string word)
+    {
+        string reversed = "";
+        char[] vogals = new char[] {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
+        
+        for(int x = word.Length - 1; x >= 0; x--)
+        {
+            if (vogals.Contains(word[x]))
+            {
+                reversed += word[x] + " ";    
+            }
+            else
+            {
+                reversed += word[x];
+            }
+        }
+
+        return reversed;
+    }
+}
