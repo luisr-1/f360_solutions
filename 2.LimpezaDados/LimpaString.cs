afe70d9030ec610acee5e275ededef73e4da173c
luisr <luisr.silva@outlook.com>
2023-01-15 21:38:21 -0300

    Adicionando novo desafio sobre limpeza de string

---
+++ 2.LimpezaDados/LimpaString.cs
@@ -0,0 +1,35 @@
+﻿
+class LimpaString
+{
+    static void Main()
+    {
+        string palavra = "CRÉDITO A VISTA - % . ,";
+        Console.WriteLine(Limpa(palavra));
+    }
+
+    static string Limpa(string palavra)
+    {
+        string limpa = "";
+        string caracteresProibidos = ".É,%-";
+        foreach (char caracter in palavra)
+        {
+            if (caracteresProibidos.Contains(caracter))
+            {
+                if (caracter == 'É')
+                {
+                    limpa += "E";
+                }
+                else
+                {
+                    limpa += "";
+                }
+            }
+            else
+            {
+                limpa += caracter;
+            }
+        }
+
+        return limpa;
+    }
+}
